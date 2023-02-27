5a8d4974eb9056e748f6cdfc96971f6b8b3f1145
matchi <77473641+matchiiii@users.noreply.github.com>
2023-02-27 09:58:16 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,34 @@
 let water = 0
+input.onButtonPressed(Button.A, function () {
+    music.setVolume(255)
+    basic.showLeds(`
+        . # . . .
+        # # . . #
+        . . # . #
+        . . # # .
+        # # . . #
+        `)
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    basic.pause(200)
+    music.playTone(175, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    basic.pause(200)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(220, music.beat(BeatFraction.Whole))
+})
 basic.forever(function () {
     water = 0
     music.setVolume(500)
